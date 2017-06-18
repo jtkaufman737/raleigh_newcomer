0cf40ed435a1c77ac3f4dfed3d5e65fbbf0fb070
J Kaufman <jtkaufman737@gmail.com>
2017-06-18 16:00:12 -0400

    format fix

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,28 +1,60 @@
 class FactsController < ApplicationController
-  def show
-    @fact = Fact.find(params[:id])
+
+def index
+ @facts=fact.all
+end
+
+def new
+  @fact = fact.new
+end
+
+def create
+  @fact=fact.new(fact_params)
+  puts fact_params
+
+  binding.pry
+  if @fact.save
+    redirect_to facts_url
+  else
+    redirect_to new_fact_url
   end
 
-  def new
-    @fact = Fact.new
+  respond_to do |format|
+  if @fact.save
+    format.html {redirect_to @fact, notice: 'New fact created'}
+  else
+    format.html {render action: 'new'}
   end
+end
+end 
 
-  def create
-    @fact = Fact.new(fact_params)
 
-    if @fact.save
-      redirect_to @fact.id
-    else
-      render 'new'
-    end
-  end
+def show
+  @fact=Fact.find(params[:id])
+end
 
-  def index
-    @facts = Fact.all
-  end
 
-  private
-    def fact_params
-      params.require(:fact).permit(:category,:body,:source)
-    end
+def edit
+  @fact = Fact.find(params[:id])
+end
+
+def update
+  fact = Fact.find(params[:id])
+  fact.update(fact_params)
+
+  redirect_to fact
+end
+
+def destroy
+  fact = Fact.find(params[:id])
+  fact.destroy
+
+  redirect_to facts_path
+end
+
+private
+def fact_params
+  params.require(:fact).permit(:body, :category, :id)
+end
+
 end
