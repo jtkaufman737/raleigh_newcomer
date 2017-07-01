eee13680cbdc4226cb4e429a40e0f379288d8f9b
J Kaufman <jtkaufman737@gmail.com>
2017-07-01 11:06:52 -0400

    route index update

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,35 +1,35 @@
 class FactsController < ApplicationController
+##  http_basic_authenticate_with name: "dhh", password: "secret", except:
+##  [:index, :show]
 
-def index
- @facts=Fact.all
-end
+  def index
+    @facts = Fact.all
+  end
 
-def new
-  @fact = Fact.new
-end
+  def show
+    @fact= Fact.find(params[:id])
+  end
 
-def create
-    @fact = Fact.new(list_params)
+  def new
+    @fact= Fact.new
+  end
+
+  def edit
+    @fact= Fact.find(params[:id])
+  end
+
+  def create
+    @fact= Fact.new(fact_params)
 
     if @fact.save
-      redirect_to @fact
+      redirect_to @Fact
     else
       render 'new'
     end
   end
 
-
-def show
-  @fact=Fact.find(params[:id])
-end
-
-
-def edit
-  @fact = Fact.find(params[:id])
-end
-
-def update
-    @fact = Fact.find(params[:id])
+  def update
+    @fact= Fact.find(params[:id])
 
     if @fact.update(fact_params)
       redirect_to @fact
@@ -38,16 +38,15 @@ def update
     end
   end
 
-def destroy
-  fact = Fact.find(params[:id])
-  fact.destroy
+  def destroy
+    @fact= Fact.find(params[:id])
+    @fact.destroy
 
-  redirect_to facts_path
-end
+    redirect_to facts_path
+  end
 
-private
-def fact_params
-  params.require(:fact).permit(:body, :category, :id)
-end
-
+  private
+    def fact_params
+      params.require(:fact).permit(:title, :text)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :facts 
+  resources :facts
 
   root 'welcome#index'
+  get "/facts", to: "facts#index"
+  get "/facts/:id", to: "facts#show"
+  get "/facts/new", to: "facts#new"
+  post "/facts", to: "facts#create"  # usually a submitted form
+  get "/facts/:id/edit", to: "facts#edit"
+  patch "/facts/:id", to: "facts#update" # usually a submitted form
+  delete "/facts/:id", to: "facts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
